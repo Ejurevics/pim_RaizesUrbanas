71f5e93a2c7ea931255f1667da32fcbb521e07f9
enzodev <enzojurevics@gmail.com>
2024-10-24 11:23:17 -0300

    update

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //master
         Scanner scanner = new Scanner(System.in);
 
         Produto produto = new Produto();
